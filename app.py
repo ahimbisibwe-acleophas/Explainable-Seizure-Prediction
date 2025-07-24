b991d3b28f295df8990a21a829e26c5a7710724f
AHIMBISIBWE ACLEOPHAS <144358177+ahimbisibwe-acleophas@users.noreply.github.com>
2025-07-24 23:46:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,18 +22,7 @@ scaler = joblib.load("scaler.pkl")
   <meta charset="UTF-8">
   <title>Seizure Prediction</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <style>
-    body {
-      background-color: #f8f9fa;
-    }
-    .container {
-      margin-top: 80px;
-    }
-    #result {
-      margin-top: 20px;
-      display: none;
-    }
-  </style>
+ 
 </head>
 <body>
   <div class="container text-center">
