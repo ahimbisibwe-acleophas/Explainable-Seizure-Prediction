29434ad71b73ae8a2835bd96c991b80ddc263d7c
AHIMBISIBWE ACLEOPHAS <144358177+ahimbisibwe-acleophas@users.noreply.github.com>
2025-08-02 20:04:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,10 @@ from lime import lime_tabular
 app = Flask(__name__)
 
 # Load the trained CNN model
-model = tf.keras.models.load_model("cnn_model_47features.h5")
+from keras.models import load_model as legacy_load_model
+model = legacy_load_model("cnn_model_47features.h5", compile=False)
+
+# model = tf.keras.models.load_model("cnn_model_47features.h5")
 EXPECTED_FEATURES = model.input_shape[-1]  # e.g., 47
 
 # HTML Template
